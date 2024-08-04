b9d7347ec8f442f8e11653bd3c61e36d5c238dfc
beopiyo <benardopiyo13@gmail.com>
2024-08-04 11:41:20 +0300

    Feat: add file to check validity of file content

---
+++ utils/validateFile.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"os"
+)
+
+// IsFileSizeWithinExpectedRanges checks if the size of the specified file matches one of the predefined valid sizes.
+func ValidateFileSize(filename string) bool {
+	file, err := os.Open(filename)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	// Get the file information
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return false
+	}
+
+	// Define valid file sizes
+	validSizes := map[int64]bool{
+		6623: true,
+		7463: true,
+		5558: true,
+	}
+
+	// Check if the file size is in the list of valid sizes
+	if validSizes[fileInfo.Size()] {
+		return true
+	}
+	return false
+}
