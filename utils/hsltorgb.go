c9f8c2bb616cc16b3e2d1b0b05e8d398c72fc95e
doonyango <datieno001@gmail.com>
2024-10-23 12:17:17 +0300

    separate concerns - to increase readability

--- utils/hsltorgb.go
+++ utils/hsltorgb.go
@@ -6,29 +6,45 @@ import (
 	"strings"
 )
 
-// HSLStringToRGB converts an hsl(h, s, l) string to an RGB array
+// converts an hsl(h, s, l) string to an RGB array.
 func HSLStringToRGB(hslStr string) ([3]int, error) {
-	var rgb [3]int
-	hslStr = strings.TrimPrefix(hslStr, "hsl(")
-	hslStr = strings.TrimSuffix(hslStr, ")")
-	parts := strings.Split(hslStr, ",")
-	if len(parts) != 3 {
-		return rgb, errors.New("invalid hsl color")
+	h, s, l, err := parseHSL(hslStr)
+	if err != nil {
+		return [3]int{}, err
 	}
-	// getting hsl values to be converted to rgb
-	h, err1 := strconv.Atoi(strings.TrimSpace(parts[0]))
-	s, err2 := strconv.Atoi(strings.TrimSpace(strings.TrimSuffix(parts[1], "%")))
-	l, err3 := strconv.Atoi(strings.TrimSpace(strings.TrimSuffix(parts[2], "%")))
-	if err1 != nil || err2 != nil || err3 != nil {
-		return rgb, errors.New("invalid hsl color")
-	}
-
 	return HSLToRGB(h, s, l), nil
 }
 
-// HSLToRGB converts HSL values to RGB values
+// extracts and converts HSL values from the hsl string.
+func parseHSL(hslStr string) (int, int, int, error) {
+	hslStr = strings.TrimPrefix(hslStr, "hsl(")
+	hslStr = strings.TrimSuffix(hslStr, ")")
+	parts := strings.Split(hslStr, ",")
+
+	if len(parts) != 3 {
+		return 0, 0, 0, errors.New("invalid hsl color")
+	}
+
+	h, err := strconv.Atoi(strings.TrimSpace(parts[0]))
+	if err != nil {
+		return 0, 0, 0, errors.New("invalid hsl color")
+	}
+
+	s, err := strconv.Atoi(strings.TrimSpace(strings.TrimSuffix(parts[1], "%")))
+	if err != nil {
+		return 0, 0, 0, errors.New("invalid hsl color")
+	}
+
+	l, err := strconv.Atoi(strings.TrimSpace(strings.TrimSuffix(parts[2], "%")))
+	if err != nil {
+		return 0, 0, 0, errors.New("invalid hsl color")
+	}
+
+	return h, s, l, nil
+}
+
+// converts HSL values to RGB values.
 func HSLToRGB(h, s, l int) [3]int {
-	var rgb [3]int
 	h = h % 360
 	s = s % 101
 	l = l % 101
@@ -37,29 +53,34 @@ func HSLToRGB(h, s, l int) [3]int {
 	x := c * (1.0 - float64(abs(h/60%2-1)))
 	m := float64(l)/100.0 - c/2.0
 
-	var r, g, b float64
-	switch {
-	case h < 60:
-		r, g, b = c, x, 0
-	case h < 120:
-		r, g, b = x, c, 0
-	case h < 180:
-		r, g, b = 0, c, x
-	case h < 240:
-		r, g, b = 0, x, c
-	case h < 300:
-		r, g, b = x, 0, c
-	default:
-		r, g, b = c, 0, x
+	r, g, b := calculateRGBComponents(float64(h), c, x)
+
+	return [3]int{
+		int((r + m) * 255.0),
+		int((g + m) * 255.0),
+		int((b + m) * 255.0),
 	}
-
-	rgb[0] = int((r + m) * 255.0)
-	rgb[1] = int((g + m) * 255.0)
-	rgb[2] = int((b + m) * 255.0)
-
-	return rgb
 }
 
+// determines RGB values based on HSL components.
+func calculateRGBComponents(h, c, x float64) (float64, float64, float64) {
+	switch {
+	case h < 60:
+		return c, x, 0
+	case h < 120:
+		return x, c, 0
+	case h < 180:
+		return 0, c, x
+	case h < 240:
+		return 0, x, c
+	case h < 300:
+		return x, 0, c
+	default:
+		return c, 0, x
+	}
+}
+
+// returns the absolute value of an integer.
 func abs(x int) int {
 	if x < 0 {
 		return -x
