561d662e2df35add3669cc2e12d095c0be818f47
doonyango <datieno001@gmail.com>
2024-10-23 11:48:34 +0300

    docs: update function document

--- utils/formatColor.go
+++ utils/formatColor.go
@@ -7,16 +7,11 @@ import (
 
 const Reset = "\033[0m"
 
-// Format the input string with the specified RGB color.
-// Returns an error message with the original string, if color not found.
-
+// formats the input string with the specified RGB color.
 func Color(text, colorName string) string {
-	// Get RGB values for the specified color
 	rgbValues, err := FindColor(strings.ToLower(colorName))
 	if err != nil {
 		return fmt.Sprintf("COLOR NOT FOUND: %s", text)
 	}
-
-	// Return the formatted string with the specified color
 	return fmt.Sprintf("\033[38;2;%d;%d;%dm%s%s", rgbValues[0], rgbValues[1], rgbValues[2], text, Reset)
 }
