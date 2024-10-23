bb3d16451337a5ba5ea0b25dfa281a8be834b459
doonyango <datieno001@gmail.com>
2024-10-23 11:35:02 +0300

    refactor: implement switch case to avoid nested ifs

--- utils/display.go
+++ utils/display.go
@@ -6,54 +6,49 @@ import (
 )
 
 func DisplayText(color string, inputslice []string, contentLines []string) string {
-	// initialize words to be colored and input
-	tobecolored := ""
-	input := ""
-	s := ""
+	var tobecolored, input string
+
 	if len(inputslice) == 2 {
 		tobecolored = inputslice[0]
 		input = inputslice[1]
 	} else {
 		tobecolored = inputslice[0]
-		input = inputslice[0]
+		input = tobecolored
 	}
-	if input == "" {
+
+	if input == "" || input == "\\n" || input == "\n" {
 		return ""
 	}
-	if input == "\\n" || input == "\n" {
-		fmt.Println()
-		return ""
-	}
+
 	input = UnescapeSpecialCharacters(input)
 	tobecolored = UnescapeSpecialCharacters(tobecolored)
 	wordslice := strings.Split(input, "\\n")
 	tbcsplit := strings.Split(tobecolored, "\\n")
 
+	var s strings.Builder
+
 	for i, word := range wordslice {
 		if word == "" {
-			s += "\n"
-		} else {
-			if IsASCII(word) && IsASCII(tobecolored) {
-				if i < len(tbcsplit) && len(tbcsplit) > 1 {
-					// coloring elements of each word on each line
-					s += PrintWord(color, word, tbcsplit[i], contentLines)
-				}
-				if i >= len(tbcsplit) && len(tbcsplit) > 1 {
-					tobecolored = ""
-					// unrepresented lines are left uncolored
-					s += PrintWord(color, word, tobecolored, contentLines)
-				}
-				// color each element in every line
-				if len(tbcsplit) == 1 {
-					s += PrintWord(color, word, tobecolored, contentLines)
-				}
-				if i != len(wordslice)-1 {
-					s += "\n"
-				}
-			} else {
-				fmt.Println("Invalid input:", word, tobecolored)
+			s.WriteString("\n")
+			continue
+		}
+
+		if IsASCII(word) && IsASCII(tobecolored) {
+			switch {
+			case i < len(tbcsplit) && len(tbcsplit) > 1:
+				s.WriteString(PrintWord(color, word, tbcsplit[i], contentLines))
+			case i >= len(tbcsplit) && len(tbcsplit) > 1:
+				s.WriteString(PrintWord(color, word, "", contentLines))
+			default:
+				s.WriteString(PrintWord(color, word, tobecolored, contentLines))
 			}
+			if i != len(wordslice)-1 {
+				s.WriteString("\n")
+			}
+		} else {
+			fmt.Println("Invalid input:", word, tobecolored)
 		}
 	}
-	return s
+
+	return s.String()
 }
